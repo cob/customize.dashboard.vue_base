520f768c083b999c5037b846c3daac092fef373d
Miguel Mesquita <miguel.a.mesquita@gmail.com>
2022-12-09 00:31:33 +0000

    feat: ask for name of dashboard

--- customize.js
+++ customize.js
@@ -1,3 +1,14 @@
 module.exports = {
-  version: "1.0.1",
+  version: "1.0.0",
+
+  questions: [{
+    type: 'input',
+    name: 'name',
+    message: 'What name for the dashboard?',
+    description: 'a valid name consist of one Word composed of letters and numbers',
+    validate: function (value) {
+        var pass = value.match( /^[-0-9a-bA-Z]+$/i );
+        return pass ? true : 'Only letters and numbers name, at least one';
+    }
+  }]
 }
